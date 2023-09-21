019c5b6e88bbe6189f70629e0ca52d45d0fa19e8
mmakovec <mmakovec23@gmail.com>
2023-09-21 13:36:23 +0200

    Dodal primere

--- primer2.py
+++ primer2.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python2
-
+import time
+from collections import deque
 
 def main():
     for i in range(15,1,-3):
@@ -13,7 +14,30 @@ def main():
         print("ind %d: %d" % (i, el))
 
     dict1 = {"k1":1, "k2":7}
-    print(dict1)
+    
+    for key, el in dict1.items():
+        print(str(el) + " " + key)
+
+    try:
+        while True:
+            print("Beseda")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print(KeyboardInterrupt)
+    except Exception as e:
+        print(e)
+    finally:
+        print("Exiting")
+
+    inp = raw_input("Vpisi svoje ime:") # python2 posebnost, navaden input vzame ime spremenljivk
+    print(inp)
+
+    d = deque(maxlen = 10)
+
+    for o in range(1,35):
+        d.append(str(o))
+
+    print(repr(d))
 
 if __name__ == "__main__":
     main()
